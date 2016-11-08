cf90f8466633d66fbfb75741c8cf03be85ac7ba2
Luis Herranz <luisherranz@gmail.com>
2016-11-08 12:27:58 +0100

    Add worona package info.

--- src/dashboard/sagas/index.js
+++ src/dashboard/sagas/index.js
@@ -1,7 +1,7 @@
 import { fork } from 'redux-saga/effects';
 
 function* logSaga() {
-  console.log('test saga running!');
+  console.log('General App package sagas running!');
 }
 
 export default function* testSagas() {
