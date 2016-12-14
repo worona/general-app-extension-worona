b2c4de56f0ad98e0ef438c96eba2d6236b78cfae
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-12-14 18:42:31 +0100

    Done redux forms components

--- src/dashboard/components/Root/index.jsx
+++ src/dashboard/components/Root/index.jsx
@@ -10,28 +10,53 @@ let GeneralSettingsForm = ({ handleSubmit, pristine, siteId, waiting }) => {
   const Input = deps.elements.Input;
   const Select = deps.elements.Select;
   return (
-      <form
+    <form
+      onSubmit={handleSubmit((values, dispatch) =>
+        dispatch(deps.actions.saveSettingsRequested(
+          { title: values.title, postNo: values.postNo },
+          { siteId, name: 'general-app-extension-worona' })))}
+    >
+      <h1>General Settings</h1>
 
+      <Field
+        name="title"
+        label="Title"
+        component={Input}
+        type="text"
+        size="medium"
+        className={`is-medium ${styles.title}`}
+      />
+      <Field
+        name="postNo"
+        label="Number of posts"
+        component={Select}
+        size="medium"
+        options={[5, 10, 15, 20, 25, 30]}
+        selected={10}
+      />
+      <br />
+      <Button
+        color="primary"
+        size="large"
+        type="submit"
+        disabled={pristine}
+        loading={waiting}
       >
-        <h1>General Settings</h1>
-
-        <Field
-          name="title"
-          label="Title"
-          component={Input}
-          type="text"
-          size="medium"
-          className={`is-medium ${styles.title}`}
-        />
-
-        <label className="label" htmlFor="postNo">Number of posts</label>
-        <p className="control" id="postNo">
-          <Select size="medium" options={[5, 10, 15, 20, 25, 30]} selected={10} />
-        </p>
-        <br />
-        <Button color="primary" size="large">Save</Button>
-      </form>
+        Save
+      </Button>
+    </form>
   );
+};
+
+GeneralSettingsForm.propTypes = {
+  handleSubmit: React.PropTypes.func.isRequired,
+  waiting: React.PropTypes.bool,
+  siteId: React.PropTypes.string,
+  pristine: React.PropTypes.bool,
+  initialValues: React.PropTypes.shape({
+    title: React.PropTypes.string,
+    postNo: React.PropTypes.number,
+  }),
 };
 
 const mapStateToFormProps = state => ({
@@ -39,7 +64,7 @@ const mapStateToFormProps = state => ({
     title: selectors.getTitle(state),
     postNo: selectors.getNumberOfPosts(state),
   },
-  // waiting: state.generalApp.SavingSettings,
+  waiting: state.settings.SavingSettings === 'general-app-extension-worona',
 });
 
 GeneralSettingsForm = reduxForm({
