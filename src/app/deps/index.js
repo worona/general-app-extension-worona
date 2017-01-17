0eac1c8c141d599cdcf0a2c01f5d59c8ab781301
Luis Herranz <luisherranz@gmail.com>
2017-01-17 17:18:56 +0100

    Refresh only on preview mode.

--- src/app/deps/index.js
+++ src/app/deps/index.js
@@ -4,6 +4,11 @@ export const types = {
   get SETTINGS_UPDATED() { return dep('settings', 'types', 'SETTINGS_UPDATED'); },
 };
 
+export const selectorCreators = {
+  get getSetting() { return dep('settings', 'selectorCreators', 'getSetting'); },
+};
+
 export const actions = {
   get postParamsChanged() { return dep('connection', 'actions', 'postParamsChanged'); },
+  get refreshPostsRequested() { return dep('connection', 'actions', 'refreshPostsRequested'); },
 };
--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -1,14 +1,25 @@
 import { takeEvery } from 'redux-saga';
-import { put } from 'redux-saga/effects';
+import { put, select } from 'redux-saga/effects';
 import * as deps from '../deps';
 
-export default function* generalAppSagas() {
+export function* updateNumberOfPosts({ fields: { numberOfPosts } }) {
   yield [
+    put(deps.actions.postParamsChanged({ params: { per_page: numberOfPosts } })),
+    put(deps.actions.refreshPostsRequested()),
+  ];
+}
+
+export default function* generalAppSagas() {
+  const initialNumberOfPosts = yield select(
+    deps.selectorCreators.getSetting('generalApp', 'numberOfPosts'),
+  );
+  yield [
+    put(deps.actions.postParamsChanged({ params: { per_page: initialNumberOfPosts } })),
     takeEvery(
-      ({ type, fields }) => type === deps.types.SETTINGS_UPDATED && fields.numberOfPosts,
-      function*({ fields: { numberOfPosts } }) {
-        yield put(deps.actions.postParamsChanged({ params: { per_page: numberOfPosts } }));
-      },
+      ({ type, fields }) =>
+        type === deps.types.SETTINGS_UPDATED && fields.numberOfPosts &&
+          fields.numberOfPosts !== initialNumberOfPosts,
+      updateNumberOfPosts,
     ),
   ];
 }
